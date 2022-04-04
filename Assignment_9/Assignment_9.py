a65d08f53de5f1f46a86676a3d624d82cfd9b105
Mayur Dongre <59500283+dongremayur777@users.noreply.github.com>
2022-04-04 14:40:40 +0530

    Histogram_Stretching

--- Assignment_9/Assignment_9.py
+++ Assignment_9/Assignment_9.py
@@ -1,29 +1,60 @@
-##DIP
-#Assignment1:
-Task1-->Read a color image and convert it into gray scale image without using inbulit function.
-Task2-->Convert the pixel of gray scale image to either 1 or 0.
-Task3-->Add gray image and image with pixels either 1 or 0 and add 20 to gray scale image.
-Performe the task and display the output images. 
+# import cv2, numpy, matplotlib
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+	
+# function to obtain histogram of an image
+def hist_plot(img):
+	
+	# empty list to store the count
+	# of each intensity value
+	count =[]
+	
+	# empty list to store intensity
+	# value
+	r = []
+	
+	# loop to traverse each intensity
+	# value
+	for k in range(0, 256):
+		r.append(k)
+		count1 = 0
+		
+		# loops to traverse each pixel in
+		# the image
+		for i in range(m):
+			for j in range(n):
+				if img[i, j]== k:
+					count1+= 1
+		count.append(count1)
+		
+	return (r, count)
 
 
-#Assignment2:
-Task-->Read a color image,convert the color image to gray scale and dispaly both images.
-Make some part of that gray scale image total black and display it.Now subtract this to images and display output image.
+img = cv2.imread('/Users/mayurdongre/Downloads/Mayur.jpeg', 0)
 
+# To ascertain total numbers of rows and
+# columns of the image, size of the image
+m, n = img.shape
+r1, count1 = hist_plot(img)
 
-#Assignment3:
-Task: Create two images one with a white Shape at center and another with a Another white Shape at center and performe all logical gate operations on both images and display the output images.
+# plotting the histogram
+plt.stem(r1, count1)
+plt.xlabel('intensity value')
+plt.ylabel('number of pixels')
+plt.title('Histogram of the original image')
 
+# Transformation to obtain stretching
+constant = (255-0)/(img.max()-img.min())
+img_stretch = img * constant
+r, count = hist_plot(img_stretch)
 
-#Assignment4:
-Task---> read a color image and display its reddish,greenisg and bluish image
+# plotting the histogram
+plt.stem(r, count)
+plt.xlabel('intensity value')
+plt.ylabel('number of pixels')
+plt.title('Histogram of the stretched image')
 
-#Assignment5:
-Task---> Read a color image and perform Histogram equalisation on it.
-
-#Assignment6:
-Task---> Read a color image and perform Contrast Enhancement on it.
-
-#Assignment7:
-Task---> Read a color image and perform bit slicing for 8 planes on it.
-
+# Storing stretched Image
+cv2.imwrite('/Users/mayurdongre/Downloads/Mayur1.jpeg', img_stretch)
