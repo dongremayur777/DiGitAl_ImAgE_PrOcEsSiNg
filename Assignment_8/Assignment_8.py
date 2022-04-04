b7e14779582760e5f8d0af09b92458b746bca729
Mayur Dongre <59500283+dongremayur777@users.noreply.github.com>
2022-04-04 14:49:06 +0530

    Update Assignment_8.py

--- Assignment_8/Assignment_8.py
+++ Assignment_8/Assignment_8.py
@@ -1,60 +1,42 @@
-# import cv2, numpy, matplotlib
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-	
-# function to obtain histogram of an image
-def hist_plot(img):
-	
-	# empty list to store the count
-	# of each intensity value
-	count =[]
-	
-	# empty list to store intensity
-	# value
-	r = []
-	
-	# loop to traverse each intensity
-	# value
-	for k in range(0, 256):
-		r.append(k)
-		count1 = 0
-		
-		# loops to traverse each pixel in
-		# the image
-		for i in range(m):
-			for j in range(n):
-				if img[i, j]== k:
-					count1+= 1
-		count.append(count1)
-		
-	return (r, count)
-
-
-img = cv2.imread('/Users/mayurdongre/Downloads/Mayur.jpeg', 0)
-
-# To ascertain total numbers of rows and
-# columns of the image, size of the image
-m, n = img.shape
-r1, count1 = hist_plot(img)
-
-# plotting the histogram
-plt.stem(r1, count1)
-plt.xlabel('intensity value')
-plt.ylabel('number of pixels')
-plt.title('Histogram of the original image')
-
-# Transformation to obtain stretching
-constant = (255-0)/(img.max()-img.min())
-img_stretch = img * constant
-r, count = hist_plot(img_stretch)
-
-# plotting the histogram
-plt.stem(r, count)
-plt.xlabel('intensity value')
-plt.ylabel('number of pixels')
-plt.title('Histogram of the stretched image')
-
-# Storing stretched Image
-cv2.imwrite('/Users/mayurdongre/Downloads/Mayur1.jpeg', img_stretch)
+Clc;
+clear all;
+close all;
+%Image Algorithm
+% Displaying original Image
+Image = imread('Fig6.38a.jpg');
+Image = rgb2gray(Image);
+imshow(I);
+title('Original Image');
+% Padding 0 to all side of the Image
+H = size(Image,1); %Height of Image
+W = size(Image,2); %Width of Image
+I_PadZero = [zeros(H,1),Image,zeros(H,1)];
+I_PadZero = [zeros(1,W+2);I_PadZero;zeros(1,W+2)];
+% Implementing the Algorithm
+I_New = zeros(H,W);
+for Row = 1:H
+for Col = 1:W
+CurrRow = Row+1; %Saving Row and Current Value
+CurrCol = Col+1;
+CurrPixel = I_PadZero(CurrRow,CurrCol);
+NewPixel = 1*(CurrPixel<I_PadZero(CurrRow,CurrCol+1))+...
+2*(CurrPixel<I_PadZero(CurrRow-1,CurrCol+1))+...
+4*(CurrPixel<I_PadZero(CurrRow-1,CurrCol))+...
+8*(CurrPixel<I_PadZero(CurrRow-1,CurrCol-1))+...
+16*(CurrPixel<I_PadZero(CurrRow,CurrCol-1))+...
+32*(CurrPixel<I_PadZero(CurrRow+1,CurrCol-1))+...
+64*(CurrPixel<I_PadZero(CurrRow+1,CurrCol))+...
+128*(CurrPixel<I_PadZero(CurrRow+1,CurrCol+1)); %Calculating new value of
+pixel
+I_New(Row,Col) = NewPixel;
+end
+end
+I_New = uint8(I_New); %Convert to Unsigned 8-Bit Data Type
+% Comparison of Original and Resultant Image
+figure(2);
+subplot(121);
+imshow(Image');
+title('Original Image');
+subplot(122);
+imshow(I_New);
+title('After Applying Algorithm');
