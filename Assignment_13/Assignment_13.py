b165fcfe9645dbcf4060415c130bb00bfab01482
Mayur Dongre <59500283+dongremayur777@users.noreply.github.com>
2022-04-05 09:43:06 +0530

    Update

--- Assignment_13/Assignment_13.py
+++ Assignment_13/Assignment_13.py
@@ -1,52 +1,29 @@
-clc;
-close all;
-clear all;
-% Toboggan Contrast Enhancement
-%% Image input and and Grad_op operator
-Grad_op = [12 14 23 13; 14 11 21 18; 21 24 23 13; 12 21 20 10]; %Gradient operator
-I = [4 4 3 2; 3 7 6 1; 2 7 6 2; 0 1 0 2]; %Image
-New = zeros(4,4); %New Matrix
-%% Algorithm
-for i = 1:4
-for j = 1:4
-min_value = Grad_op(i,j);
-x = i;
-y = j;
-if(i+1 <= 4 && Grad_op(i+1,j) < min_value)
-min_value = Grad_op(i+1,j);
-x = i+1;
-y = j;
-end
-if(i-1 >= 1 && Grad_op(i-1,j) < min_value)
-min_value = Grad_op(i-1,j);
-x = i-1;
-y = j;
-end
-if(j-1 >= 1 && Grad_op(i,j-1) < min_value)
-min_value = Grad_op(i,j-1);
-y = j - 1;
-x = i;
-end
-if(j+1 <= 4 && Grad_op(i,j+1) < min_value)
-min_value = Grad_op(i,j+1);
-y = j + 1;
-x = i;
-end
-if(j+1 <= 4 && i+1 <= 4 && Grad_op(i+1,j+1) < min_value)
-min_value = Grad_op(i+1,j+1);
-y = j+1;
-x = i+1;
-end
-if(j-1>= 1 && i+1 <= 4 && Grad_op(i+1,j-1) <min_value)
-min_value = Grad_op(i+1,j-1);
-y = j-1;
-x = i+1;
-end
-if(j + 1 <= 4 && i - 1 >= 1 && Grad_op(i-1,j+1) < min_value)
-min_value = Grad_op(i-1,j+1);
-y = j + 1;
-x = i - 1;
-end
-New(i,j) = I(x,y);
-end
-end
+##DIP
+#Assignment1:
+Task1-->Read a color image and convert it into gray scale image without using inbulit function.
+Task2-->Convert the pixel of gray scale image to either 1 or 0.
+Task3-->Add gray image and image with pixels either 1 or 0 and add 20 to gray scale image.
+Performe the task and display the output images. 
+
+
+#Assignment2:
+Task-->Read a color image,convert the color image to gray scale and dispaly both images.
+Make some part of that gray scale image total black and display it.Now subtract this to images and display output image.
+
+
+#Assignment3:
+Task: Create two images one with a white Shape at center and another with a Another white Shape at center and performe all logical gate operations on both images and display the output images.
+
+
+#Assignment4:
+Task---> read a color image and display its reddish,greenisg and bluish image
+
+#Assignment5:
+Task---> Read a color image and perform Histogram equalisation on it.
+
+#Assignment6:
+Task---> Read a color image and perform Contrast Enhancement on it.
+
+#Assignment7:
+Task---> Read a color image and perform bit slicing for 8 planes on it.
+
