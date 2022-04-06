d56e9983d5ea8bcb5acd96e4ad9a0425135e36d0
Mayur Dongre <59500283+dongremayur777@users.noreply.github.com>
2022-04-06 22:29:26 +0530

    Edge Detection

--- Assignment_16/Assignment_16.py
+++ Assignment_16/Assignment_16.py
@@ -1,29 +1,21 @@
-##DIP
-#Assignment1:
-Task1-->Read a color image and convert it into gray scale image without using inbulit function.
-Task2-->Convert the pixel of gray scale image to either 1 or 0.
-Task3-->Add gray image and image with pixels either 1 or 0 and add 20 to gray scale image.
-Performe the task and display the output images. 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
 
+# read the image
+image = cv2.imread(sys.argv[1])
 
-#Assignment2:
-Task-->Read a color image,convert the color image to gray scale and dispaly both images.
-Make some part of that gray scale image total black and display it.Now subtract this to images and display output image.
+# convert it to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# show the grayscale image, if you want to show, uncomment 2 below lines
+# plt.imshow(gray, cmap="gray")
+# plt.show()
 
-#Assignment3:
-Task: Create two images one with a white Shape at center and another with a Another white Shape at center and performe all logical gate operations on both images and display the output images.
+# perform the canny edge detector to detect image edges
+edges = cv2.Canny(gray, threshold1=30, threshold2=100)
 
-
-#Assignment4:
-Task---> read a color image and display its reddish,greenisg and bluish image
-
-#Assignment5:
-Task---> Read a color image and perform Histogram equalisation on it.
-
-#Assignment6:
-Task---> Read a color image and perform Contrast Enhancement on it.
-
-#Assignment7:
-Task---> Read a color image and perform bit slicing for 8 planes on it.
-
+# show the detected edges
+plt.imshow(edges, cmap="gray")
+plt.show()
