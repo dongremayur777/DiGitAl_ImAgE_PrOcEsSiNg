ffcb50e9a754789381bda93948f1cf82224c3f7f
Mayur Dongre <59500283+dongremayur777@users.noreply.github.com>
2022-04-06 22:41:09 +0530

    Update Assignment_17.py

--- Assignment_17/Assignment_17.py
+++ Assignment_17/Assignment_17.py
@@ -1,29 +1,23 @@
-##DIP
-#Assignment1:
-Task1-->Read a color image and convert it into gray scale image without using inbulit function.
-Task2-->Convert the pixel of gray scale image to either 1 or 0.
-Task3-->Add gray image and image with pixels either 1 or 0 and add 20 to gray scale image.
-Performe the task and display the output images. 
-
-
-#Assignment2:
-Task-->Read a color image,convert the color image to gray scale and dispaly both images.
-Make some part of that gray scale image total black and display it.Now subtract this to images and display output image.
-
-
-#Assignment3:
-Task: Create two images one with a white Shape at center and another with a Another white Shape at center and performe all logical gate operations on both images and display the output images.
-
-
-#Assignment4:
-Task---> read a color image and display its reddish,greenisg and bluish image
-
-#Assignment5:
-Task---> Read a color image and perform Histogram equalisation on it.
-
-#Assignment6:
-Task---> Read a color image and perform Contrast Enhancement on it.
-
-#Assignment7:
-Task---> Read a color image and perform bit slicing for 8 planes on it.
-
+clc; close all; clear all;
+% Delta Modulation
+function [y MSE]=Delta_Modulation(del, A) %del=step size, A=amplitude of signal
+T=0:2*pi/100:2*pi;
+x=A*sin(T);
+plot(x)
+hold on
+y=[0]; %y=output binary sequence
+xr=0;
+for i=1:length(x)-1
+if xr(i)<=x(i)
+d=1;
+xr(i+1)=xr(i)+del;
+else
+d=0;
+xr(i+1)=xr(i)-del;
+end
+y=[y d];
+end
+stairs(xr)
+hold off
+MSE=sum((x-xr).^2)/length(x);
+End
